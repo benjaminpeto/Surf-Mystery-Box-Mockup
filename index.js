ddb9a97681a45ba96392512462a9dd65c984b955
Benji <66526461+benjaminpeto@users.noreply.github.com>
2021-05-25 17:43:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function sorry(){
+    alert("I know you love them, but unfortunately, it is just a mock up page 😭 Have a wonderful day 🤙🏽");
+};
+function almost(){
+    alert("Sorry, it's not working. You might have to get a new computer 😂 Just kidding... keep looking please.");
+};
+function submitted(){
+    alert("Thank you for your data. We will not hack you, I promise. 🤍")
+};
